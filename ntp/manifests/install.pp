f6c092f7dbb3dc3ec7a6baa174cb94b5e6465019
root <root@jabreu081.mylabserver.com>
2018-02-26 19:27:17 +0000

    First commit

---
+++ ntp/manifests/install.pp
@@ -0,0 +1,9 @@
+class ntp::install(
+  String $package_name   = $ntp::package_name,
+  String $package_ensure = $ntp::package_ensure,
+
+) {
+  package { $package_name:
+    ensure => $package_ensure,
+  }
+}
